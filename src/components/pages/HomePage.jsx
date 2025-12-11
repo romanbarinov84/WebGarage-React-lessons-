fecd571c1f388080e22d44ecdcd7cd0bc0bcb468
Roman Meinl <roman-meinl@ukr.net>
2025-12-11 10:49:08 -0200

    11.12 les - 1.22.1

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -5,9 +5,16 @@ import "./HomePage.css";
 
 const HomePage = () => {
   return (
-    <div className="Home-Page">
-   
+    <div className="flex flex-col items-center justify-start bg-gray-200 w-full rounded-2xl px-4 py-8 min-h-[calc(100vh-160px)] border-4 border-amber-300">
+     
+     <h1 className="text-3xl md:text-4xl font-bold text-center mb-6">Home Page Component</h1>
+     <p className="text-base md:text-lg text-gray-700 leading-relaxed text-center mb-4">This is central conteiner</p>
       
+       <div>
+        <div>One</div>
+        <div>Two</div>
+        <div>Three</div>
+       </div>
     </div>
   );
 };
