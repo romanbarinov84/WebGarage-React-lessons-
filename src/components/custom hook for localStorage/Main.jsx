135ccfc67f9ac25f5b183880927121d4d67e30b5
Roman Meinl <roman-meinl@ukr.net>
2026-02-19 12:54:53 +0200

    19.02 create useLocalStorage.js

--- src/components/custom hook for localStorage/Main.jsx
+++ src/components/custom hook for localStorage/Main.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react"
 
 
 const Main = () => {
+     
+  
+   
   return (
     <div>
-     <h1>Hello </h1>
+    <h1>Hello : <spin style={{color:"red",fontSize:"24px",fontWeight:"bold"}}>{user}</spin></h1>
+    <input 
+    type="text" 
+    value={user}
+    onChange={(e) => handleSetUser(String(e.target.value))}
+    placeholder="Enter your name!!!1"
+    style={{border:"2px solid black"}}
+    />
 
     </div>
   )
